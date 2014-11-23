8ee97d2623a4fc22ea03e9aabb11f8898c7d3498
vallai <chrisk8er88@yahoo.fr>
2014-11-23 15:43:11 +0100

    Initialisation

---
+++ Location/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Location.Startup))]
+namespace Location
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
