f70d18c46f2e2f5d3120a89ffccda615b92305b5
vallai <chrisk8er88@yahoo.fr>
2014-11-24 21:20:31 +0100

    Initialisation de la base de données

--- Location/Global.asax.cs
+++ Location/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Location.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,7 @@ namespace Location
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<ApplicationDbContext>(new Initializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ Location/Models/Initializer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Location.Models
+{
+    //public class MagasinInitializer : DropCreateDatabaseIfModelChanges<MagasinContext>
+    // CreateDatabaseIfNotExists
+    // DropCreateDatabaseAlways
+    public class Initializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context)); 
+
+            var appUser = new ApplicationUser();
+            appUser.UserName = "a@a.fr";
+            appUser.Email = "a@a.fr";
+            appUser.prenom = "Christian";
+            appUser.nom = "SCHMIDT";
+            var utilisateur = UserManager.Create(appUser, "azerty");
+            base.Seed(context);
+
+
+            context.SaveChanges();
+
+        }
+    }
+}
