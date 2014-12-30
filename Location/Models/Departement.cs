83478a6fde64c603246c8515eaba58098084a4c3
vallai <chrisk8er88@yahoo.fr>
2014-12-30 01:16:50 +0100

    Ajout du favicon

---
+++ Location/Models/Departement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Location.Models
+{
+    [Table("Departements")]
+    public class Departement
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public int Numero { get; set; }
+        [Required]
+        public virtual Region Region { get; set; }
+    }
+}
